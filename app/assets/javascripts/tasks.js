2994b84aba42ae0a4dcebd0f022c4ca3c6ee13c6
Travis Fantina <tfantina@gmail.com>
2019-02-01 07:01:39 -0500

    Adds a slight transition to 'new-task-form'

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,24 +1,25 @@
-
-
 function showForm() {
-  const btn = document.getElementById('new-task-form')
-  btn.style.display == "block" ? btn.style.display = "none" : btn.style.display = "block"
+  const btn = document.getElementById("new-task-form");
+  btn.style.height == "100%"
+    ? ((btn.style.height = "0px"), (btn.style.opacity = "0"))
+    : ((btn.style.height = "100%"), (btn.style.opacity = "1"));
 }
 
 jQuery(
-
-  $(document).on('turbolinks:load', function() {
-  let subtasks;
-  subtasks = $('#task_subproject_id').html();
-  return $('#task_project_id').change(function(){
-    let project, optons;
-    project = $('#task_project_id :selected').text();
-    options = $(subtasks).filter("optgroup[label='" + project + "']").html();
-    if(options) {
-      return $('#task_subproject_id').html(options);
-    } else {
-      return $('#task_subproject_id').empty();
-    }
-  });
-})
+  $(document).on("turbolinks:load", function() {
+    let subtasks;
+    subtasks = $("#task_subproject_id").html();
+    return $("#task_project_id").change(function() {
+      let project, optons;
+      project = $("#task_project_id :selected").text();
+      options = $(subtasks)
+        .filter("optgroup[label='" + project + "']")
+        .html();
+      if (options) {
+        return $("#task_subproject_id").html(options);
+      } else {
+        return $("#task_subproject_id").empty();
+      }
+    });
+  })
 );
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190116115411) do
+ActiveRecord::Schema.define(version: 20190131115503) do
 
   create_table "project_assocaitions", force: :cascade do |t|
     t.integer  "project_id"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20190116115411) do
     t.integer  "subproject_id"
     t.index ["project_id"], name: "index_tasks_on_project_id"
     t.index ["subproject_id"], name: "index_tasks_on_subproject_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "lead"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_tasks", force: :cascade do |t|
