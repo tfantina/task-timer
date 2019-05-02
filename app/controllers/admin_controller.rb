67c8746413b3429d5c47cc25e3eecaa5b08561c8
tfantina <tfantina@gmail.com>
2019-05-02 11:26:15 -0400

    Adds favicon

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,8 @@ class AdminController < ApplicationController
   def index
       @users = User.order(name: :asc).all
   end
+
+  def user_projects
+    @tasks = Task.where(user_id: id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :subprojects, only: [:new, :create]
   resources :admin, only: [:index]
+  get 'admin/user_projects/:id', to: 'admin#user_projects'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
